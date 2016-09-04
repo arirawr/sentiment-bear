ccc1d71e86186c1e92900d46bdcad0f6a06deb4f
Chris Roth <chris@cjroth.com>
2016-09-04 15:28:51 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const input = 'journals/Journal.json'
+const output = 'results/journal.csv'
+
+const fs = require('fs')
+const path = require('path')
+const sentiment = require('sentiment')
+const json2csv = require('json2csv')
+
+const journalPath = path.resolve(input)
+const entries = JSON.parse(fs.readFileSync(journalPath)).entries
+
+// console.log(sentiment(entries[0].text))
+
+let results = []
+
+entries.forEach((entry) => {
+    let result = {
+        uuid: entry.uuid,
+        date: entry.creationDate,
+        sentiment: sentiment(entry.text).score,
+        weather: entry.weather,
+        location: entry.location
+    }
+    results.push(result)
+})
+
+const fields = ['date', 'sentiment', 'weather.temperatureCelsius', 'weather.weatherCode', 'location.localityName']
+
+let csv = json2csv({ data: results, fields: fields })
+
+fs.writeFileSync(output, csv)
